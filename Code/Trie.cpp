129ad0629f77287baec58297788e3afc62b4e738
stefan <stefanronczka@outlook.de>
2014-12-02 12:32:53 +0100

    Projekt ist eingebunden

---
+++ Code/Trie.cpp
@@ -0,0 +1,21 @@
+/*
+ * Trie.cpp
+ *
+ *  Created on: 02.12.2014
+ *      Author: Stefan
+ */
+
+#include "Trie.h"
+
+namespace PA2 {
+
+Trie::Trie() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Trie::~Trie() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace PA2 */
---
+++ Code/Trie.h
@@ -0,0 +1,21 @@
+/*
+ * Trie.h
+ *
+ *  Created on: 02.12.2014
+ *      Author: Stefan
+ */
+
+#ifndef TRIE_H_
+#define TRIE_H_
+
+namespace PA2 {
+
+class Trie {
+public:
+	Trie();
+	virtual ~Trie();
+};
+
+} /* namespace PA2 */
+
+#endif /* TRIE_H_ */
